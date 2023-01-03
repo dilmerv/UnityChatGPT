05e1fe8eb3c8b5843c5a139acf8f10442eee4a0c
Dilmer Valecillos <dilmervst@gmail.com>
2023-01-02 17:49:29 -0700

    Added a scroll view for times when the code is too long

--- Assets/Scripts/ChatGPT/ChatGPTExtensions.cs
+++ Assets/Scripts/ChatGPT/ChatGPTExtensions.cs
@@ -2,15 +2,14 @@
 
 public static class ChatGPTExtensions
 {
-    public static string[] keywords = new string[]{ "Apply()", "using UnityEngine" };
+    public const string KEYWORD_APPLY = "Apply()";
+    public const string KEYWORD_USING = "using UnityEngine";
+    public const string KEYWORD_PUBLIC_CLASS = "public class";
 
     public static ChatGPTResponse CodeCleanUp(this ChatGPTResponse chatGPTResponse)
     {
         var codelines = chatGPTResponse.Data.Split("```");
-        foreach(var codeLine in codelines)
-        {
-            
-        }
+        chatGPTResponse.Data = codelines.FirstOrDefault(c => c.Contains(KEYWORD_APPLY) || c.Contains(KEYWORD_USING) || c.Contains(KEYWORD_PUBLIC_CLASS));
         return chatGPTResponse;
     }
 
